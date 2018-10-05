bb00d55b140beb263187f5f5a931ead65f3a33e7
Rohurd <rohurd@gmx.net>
2018-10-05 20:52:01 +0200

    - rotation added

--- Scripts/Square.gd
+++ Scripts/Square.gd
@@ -11,14 +11,17 @@ func _ready():
 func _physics_process(delta):
 	var velocity = Vector2() # The player's movement vector.
 	var projectResolution=get_viewport().size
-	if Input.is_action_pressed("square_right") && position.x <= projectResolution.x - radius:
+	if Input.is_action_pressed("square_right"):
 		velocity.x += 1
-	if Input.is_action_pressed("square_left") && position.x >= radius:
+	if Input.is_action_pressed("square_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("square_down") && position.y <= projectResolution.y - radius:
+	if Input.is_action_pressed("square_down"):
 		velocity.y += 1
-	if Input.is_action_pressed("square_up") && position.y >= radius:
+	if Input.is_action_pressed("square_up"):
 		velocity.y -= 1
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
+		look_at(position + velocity)
 	move_and_slide(velocity)
+	position.x = clamp(position.x, radius, projectResolution.x - radius)
+	position.y = clamp(position.y, radius, projectResolution.y - radius)
--- Scripts/Triangle.gd
+++ Scripts/Triangle.gd
@@ -1,8 +1,5 @@
 extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 export var speed = 250
 export var radius = 15
 
@@ -13,15 +10,18 @@ func _ready():
 
 func _physics_process(delta):
 	var velocity = Vector2() # The player's movement vector.
-	var projectResolution=Vector2(get_viewport().size)
-	if Input.is_action_pressed("triangle_right") && position.x <= projectResolution.x - radius:
+	var projectResolution=get_viewport().size
+	if Input.is_action_pressed("triangle_right"):
 		velocity.x += 1
-	if Input.is_action_pressed("triangle_left") && position.x >= radius:
+	if Input.is_action_pressed("triangle_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("triangle_down") && position.y <= projectResolution.y - radius:
+	if Input.is_action_pressed("triangle_down"):
 		velocity.y += 1
-	if Input.is_action_pressed("triangle_up") && position.y >= radius:
+	if Input.is_action_pressed("triangle_up"):
 		velocity.y -= 1
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
+		look_at(position + velocity)
 	move_and_slide(velocity)
+	position.x = clamp(position.x, radius, projectResolution.x - radius)
+	position.y = clamp(position.y, radius, projectResolution.y - radius)
