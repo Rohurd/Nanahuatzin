cba8e9eb53fc64a6ee5ab4361656da370e3ca42b
Rohurd <rohurd@gmx.net>
2018-10-05 19:19:02 +0200

    - Initial commit, Square + movement working

---
+++ Scripts/Square.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+export var speed = 1
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _process(delta):
+	var velocity = Vector2() # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	position += velocity * delta
